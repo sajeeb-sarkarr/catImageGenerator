cd96b662bc53b844c51d451c6a60190c225c96a0
sajeeb20 <148118432+sajeeb20@users.noreply.github.com>
2023-10-17 00:47:07 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,29 @@
+const customCursor = document.querySelector(".custom-cursor");
+const catImage = document.getElementById('catImage');
+
+document.addEventListener("mousemove", (e) => {
+    // Update the custom cursor position to follow the mouse
+    customCursor.style.left = e.clientX + "px";
+    customCursor.style.top = e.clientY + "px";
+});
+
+function fetchingImage() {
+    const apiKey = 'live_LeSuArGyMeVuHrQvvK1RQ2DIz6XaqIs6f6o7g7rXJOH7hvaCathfYqGGIBmQl4lz';
+    const apiUrl = "https://api.thecatapi.com/v1/images/search?q=British%20Longhair";
+
+    fetch(apiUrl, {
+        "method": "GET",
+        "headers": { 'x-api-key': apiKey }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let imageUrl = data[0].url;
+
+            document.getElementById("image").innerHTML = `<img src="${imageUrl}" width=300 height=360>`
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+}
